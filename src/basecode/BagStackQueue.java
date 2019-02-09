6f4745d1a67631966e486ad8d261cb7cbae9becc
fancyChuan <1247375074@qq.com>
2019-02-09 23:50:34 +0800

    栈、队列用例

--- src/basecode/BagStackQueue.java
+++ src/basecode/BagStackQueue.java
@@ -1,9 +1,10 @@
 package basecode;
 
-import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.*;
 
+/**
+ * 背包、栈、队列的用例
+ */
 public class BagStackQueue {
 
     public static void main(String[] args) {
@@ -16,5 +17,29 @@ public class BagStackQueue {
             numbers.add(StdIn.readDouble());
         }
         StdOut.println("this bag has " + numbers.size() + "items");
+    }
+
+    public static int[] queueCase(String name) {
+        In in = new In(name);
+        Queue<Integer> q = new Queue<Integer>();
+        while (!in.isEmpty()) {
+            q.enqueue(in.readInt());
+        }
+        int N = q.size();
+        int[] a = new int[N];
+        for (int i=0; i<N; i++) {
+            a[i] = q.dequeue();
+        }
+        return a;
+    }
+
+    public static void stackCase() {
+        Stack<Integer> stack = new Stack<Integer>();
+        while (!StdIn.isEmpty()) {
+            stack.push(StdIn.readInt());
+        }
+        for (int i: stack) {
+            StdOut.println(i);
+        }
     }
 }
