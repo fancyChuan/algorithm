22210cb2f4c7f27c937c0d26cf790bca1a598eb6
hp-fancy <1247375074@qq.com>
2021-09-14 18:05:09 +0800

    数据结构与算法面试宝典：队列-FIFO-示例1-解法2

--- src/main/java/lagou/model1/QueueSolution.java
+++ src/main/java/lagou/model1/QueueSolution.java
@@ -2,6 +2,7 @@ package lagou.model1;
 
 import lagou.common.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -9,7 +10,9 @@ import java.util.Queue;
 public class QueueSolution {
 
     /**
-     *  例 1：二叉树的层次遍历（两种方法）
+     *  例 1：二叉树的层次遍历，解法一
+     *  由于二叉树的每个结点，我们都只访问了一遍，所以时间复杂度为 O(n)。
+     *  如果不算返回的数组，那么空间复杂度为 O(k)，这里的 k 表示二叉树横向最宽的那一层的结点数目。
      */
     public static List<List<Integer>> levelOrder(TreeNode root) {
         // 生成FIFO队列
@@ -40,6 +43,38 @@ public class QueueSolution {
             }
             // 遍历完了当层的内容，写入结果
             ans.add(levelValue);
+        }
+        return ans;
+    }
+
+    /**
+     * 例 1：二叉树的层次遍历，解法二
+     *  采用两层ArrayList来表示
+     */
+    public static List<List<Integer>> levelOrder2(TreeNode root) {
+        // 最终的结果
+        List<List<Integer>> ans = new LinkedList<>();
+        // 新建一个用来存当前节点的列表
+        ArrayList<TreeNode> curLevel = new ArrayList<>();
+        // 根结点的初始化
+        if (root != null) {
+            curLevel.add(root);
+        }
+
+        while (curLevel.size() > 0) {
+            ArrayList<Integer> curResult = new ArrayList<>();
+            ArrayList<TreeNode> nextLevel = new ArrayList<>();
+            for (TreeNode treeNode : curLevel) {
+                if (treeNode.left != null) {
+                    nextLevel.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    nextLevel.add(treeNode.right);
+                }
+                curResult.add(treeNode.value);
+            }
+            curLevel = nextLevel;
+            ans.add(curResult);
         }
         return ans;
     }
