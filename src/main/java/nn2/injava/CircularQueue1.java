63acf88abd4969073720099ff274bfd56a15ccfc
hp-fancy <1247375074@qq.com>
2021-09-28 07:54:21 +0800

    数据结构与算法面试宝典：队列-FIFO-示例1-思考题

--- src/main/java/nn2/injava/CircularQueue1.java
+++ src/main/java/nn2/injava/CircularQueue1.java
@@ -1,14 +1,20 @@
 package nn2.injava;
 
-import nn2.injava.queue.MyQueue;
-
 /**
  * 循环队列的实现
  *  使用了used来代表已使用的空间
+ * 理解：
+ *  1. 队列是一种逻辑上的规定，只允许某种操作。比如这个类中，使用了 int[] 数组
+ *     int[]是可以通过下标来随机读取和更新的，但是顺序队列/循环队列都规定了只能从队头取元素，也就是“先进先出”
+ *  2. 对于顺序队列来说，随着front的不断“前进”，int[]的前面几个空间是得不到利用的，并且当rear=capacity的时候就出现了“假溢出”
+ *  3. 队头出元素了之后，int[]实际上还是存了元素的，这是规定了按front读取，而front只能往前加
+ *  4. 在实际上，顺序队列用得非常少，绝大部分都会使用循环队列
  */
 public class CircularQueue1 {
     private int used = 0;
+    // 指向队头元素
     private int front = 0;
+    // 指向队尾元素的下一个位置
     private int rear = 0;
     private int[] a;
     private int capacity = 0;
@@ -19,7 +25,7 @@ public class CircularQueue1 {
         a = new int[capacity];
     }
 
-    // 将value放到队列中, 成功返回true
+    // 将value放到队列中, 成功返回true。放在队尾
     public boolean enQueue(int value) {
         if (isFull()) {
             return false;
@@ -47,6 +53,7 @@ public class CircularQueue1 {
         return a[front];
     }
     // 得到队尾元素，如果队列为空，返回-1
+    // 注意这个方法不会让元素出队列，所以指针front和rear是不变的
     public int Rear() {
         if (isEmpty()) {
             return -1;
@@ -58,7 +65,6 @@ public class CircularQueue1 {
     public int getSize() {
         return used;
     }
-
     // 看一下循环队列是否为空
     public boolean isEmpty() {
         return used == 0;
