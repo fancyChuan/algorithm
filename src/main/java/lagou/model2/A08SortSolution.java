917f8f2657c35256f2e5519b2d1208331f4c97f1
hp-fancy <1247375074@qq.com>
2021-11-17 22:46:48 +0800

    排序：归并排序-实现逆序对

--- src/main/java/lagou/model2/A08SortSolution.java
+++ src/main/java/lagou/model2/A08SortSolution.java
@@ -1,6 +1,8 @@
 package lagou.model2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class A08SortSolution {
     /**
@@ -43,5 +45,41 @@ public class A08SortSolution {
         int[] tmp = new int[nums.length];
         msort(nums, 0, nums.length, tmp);
     }
+    /**
+     * 例 1：逆序对
+     * https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/submissions/
+     *
+     * 归并排序的特点：
+     *  1. j永远是大于i的。  对于这个题目来说，使用归并排序就变成了只要判断a[i]是否大于a[j]
+     *  2. 在排序的过程中，有3个地方是有有序的。左子序列、右子序列、tmp[]中间结果集。
+     *      对于这道题目来说，就是用到了tmp[]的结果是有序的。也就是说，在比较a[i]和a[j]的时候，[m,j)的数据已经排到tmp[]中了
+     *      这样一来，a[i]就和[m,j)的所有元素都构成逆序对！
+     */
+    private int cnt1;
+    private void m1Sort(int[] arr, int start, int end, int[] tmp) {
+        if (start >= end || start+1 >= end) {
+            return;
+        }
+        int mid = start + ((end - start)>>1);
+        int i = start;
+        int j = mid;
+        int to = start;
+        if (j>=end || i<mid && arr[i]<=arr[j]) {
+            tmp[to++] = arr[i++];
+            cnt1 += 1; // 最核心的就是在这个位置写的这一行代码！
+        } else {
+            tmp[to++] = arr[j++];
+        }
+    }
+    public int reversePairs(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+        cnt1 = 0;
+        int[] tmp = new int[nums.length];
+        m1Sort(nums, 0, nums.length, tmp);
+        return cnt1;
+    }
+
 
 }
