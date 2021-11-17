f6dfb14865e9643671b5b1dea8233fcc1e163391
hp-fancy <1247375074@qq.com>
2021-11-18 00:22:50 +0800

    排序：归并排序-链表的归并排序

--- src/main/java/lagou/model2/A08SortSolution.java
+++ src/main/java/lagou/model2/A08SortSolution.java
@@ -1,5 +1,7 @@
 package lagou.model2;
 
+import lagou.common.ListNode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -49,6 +51,67 @@ public class A08SortSolution {
         int[] tmp = new int[nums.length];
         msort(nums, 0, nums.length, tmp);
     }
+
+    /**
+     * 思考题：给定一个链表，如何排序，使其时间复杂度能够达到 O(NlogN)。空间复杂度为 O(1)
+     *      https://leetcode-cn.com/problems/sort-list/description/
+     * 关键点：
+     *  1. 找到链表的中间节点：使用快慢双指针
+     *  2. 拆成两个链表的时候，注意断开！（也就是要在middle.next=null）
+     *  3. 使用空头链表，作为合并结果
+     *
+     * 注意链表的归并排序和数组的归并排序的对比：
+     *  1. 中间节点：链表要用快慢指针，数组int m = start + ((end - start)>>1)
+     *  2. 归并中间结果：链表使用空头新链表，并返回。而数组使用tmp[]，不返回，但通过覆盖写入arr的方式
+     *  3. 稳定排序：链表通过next指针是否为空来判断，而数组通过下标是否超过[start,mid) 和 [mid,end)来判断
+     */
+    // 找中间节点
+    private ListNode findMiddle(ListNode head) {
+        ListNode s1 = head;
+        ListNode s2 = head;
+        ListNode pre = head;
+        while (s2 != null && s2.next != null) {
+            pre = s1;
+            s1 = s1.next;
+            s2 = s2.next.next;
+        }
+        // 注意奇偶个数的链表的不同情况判断
+        return s2 != null ? s1 : pre;
+    }
+    // 递归排序的方法
+    private ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // 找到中间节点之后，需要拆成2个链表。因此要注意通过 middle.next=null 与back断开
+        ListNode middle = findMiddle(head);
+        ListNode back = middle.next;
+        middle.next = null;
+        // 递归排序
+        ListNode left = mergeSort(head);
+        ListNode right = mergeSort(back);
+        // 进行合并，使用空头链表
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        while (left != null || right != null) {
+            if (right == null || left != null && left.val <= right.val) {
+                tail.next = left;
+                tail = left;
+                left = left.next;
+            } else {
+                tail.next = right;
+                tail = right;
+                right = right.next;
+            }
+        }
+        tail.next = null; // TODO：这行代码是否有必要？
+        // 返回合并后的结果
+        return dummy.next;
+    }
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+
     /**
      * 例 1：逆序对
      * https://leetcode-cn.com/problems/shu-zu-zhong-de-ni-xu-dui-lcof/submissions/
@@ -92,6 +155,21 @@ public class A08SortSolution {
         m1Sort(nums, 0, nums.length, tmp);
         return cnt1;
     }
+    /**
+     * 例1 扩展题 [315] 计算右侧小于当前元素的个数
+     *  https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/description/
+     */
+    private void m2Sort(int[] nums, int start, int end, int[] tmp) {
+        if (start >= end || start+1 >= end) {
+            return;
+        }
+        int m = start + ()
+    }
+    public List<Integer> countSmaller(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
 
+
+        return ans;
+    }
 
 }
